49c735738eb658ab980566b8ff87d9278300d2ef
William M <cominatyou@cominatyou.com>
2024-02-07 21:48:09 -0600

    Fix production domain using malformed URL

--- Launchpad/NotificationHistoryFetcher.swift
+++ Launchpad/NotificationHistoryFetcher.swift
@@ -1,12 +1,15 @@
 import Foundation
 import Alamofire
+import UIKit
 
 enum RequestError: Error {
     case failed
 }
 
 func fetchNotificationHistory(after: Double?) async throws -> Data {
-    let request = AF.request("http://\(HISTORY_ENDPOINT_DOMAIN)/notification-history\(after != nil ? String(format: "?after=%.0f", after!) : "")", headers: ["Authorization": "Token \(HISTORY_AUTH_TOKEN)"])
+    let device = await UIDevice.current.userInterfaceIdiom == .phone ? "iphone" : "ipad"
+
+    let request = AF.request("\(HISTORY_ENDPOINT_DOMAIN)/notifications/history?device=\(device)\(after != nil ? String(format: "&after=%.0f", after!) : "")", headers: ["Authorization": "Token \(HISTORY_AUTH_TOKEN)"])
     let response = await request.serializingString().response
     
     if let error = response.error {
